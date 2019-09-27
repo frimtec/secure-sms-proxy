5e2162a32cc9c2984a255c4acfc29d765509f9c2
Markus Friedli <frimtec@gmx.ch>
2019-09-27 22:46:03 +0200

    Layout register activity

--- app/src/main/java/com/github/frimtec/android/securesmsproxy/activity/MainActivity.java
+++ app/src/main/java/com/github/frimtec/android/securesmsproxy/activity/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-    menu.add(Menu.NONE, MENU_CONTEXT_DELETE_ID, Menu.NONE, "Delete");
+    menu.add(Menu.NONE, MENU_CONTEXT_DELETE_ID, Menu.NONE, R.string.action_delete);
   }
 
   @Override
