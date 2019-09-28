35fa3fa0c4007f18e6efcf1cbcedb4459ccfd48d
Markus Friedli <frimtec@gmx.ch>
2019-09-28 09:46:29 +0200

    Launcher icon changed

--- app/src/main/java/com/github/frimtec/android/securesmsproxy/activity/AboutActivity.java
+++ app/src/main/java/com/github/frimtec/android/securesmsproxy/activity/AboutActivity.java
@@ -49,7 +49,7 @@ public class AboutActivity extends AppCompatActivity {
     }
 
     textView.setText(Html.fromHtml(
-        "<h2><a href='https://github.com/frimtec/secure-sms-proxy'>S2MSP</a></h2>" +
+        "<h2><a href='https://github.com/frimtec/secure-sms-proxy'>Secure SMS Proxy</a></h2>" +
             "<p>Version: " + version + " (Build  " + build + ")</p>" +
             "<p>&copy; 2019 <a href='https://github.com/frimtec'>frimTEC</a></p>" +
             ""
