d5e75f2ecca0250b6e930ca1b61ae9a46b47a883
Markus Friedli <frimtec@gmx.ch>
2019-12-14 22:21:07 +0100

    Add unit tests

---
+++ app/src/test/java/com/github/frimtec/android/securesmsproxy/state/DbHelperTest.java
@@ -0,0 +1,37 @@
+package com.github.frimtec.android.securesmsproxy.state;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static com.github.frimtec.android.securesmsproxy.state.DbHelper.TABLE_APPLICATION;
+import static com.github.frimtec.android.securesmsproxy.state.DbHelper.TABLE_RULE;
+import static com.github.frimtec.android.securesmsproxy.state.DbHelper.VIEW_APPLICATION_RULE;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class DbHelperTest {
+
+  @Test
+  public void onCreate() {
+    DbHelper dbHelper = new DbHelper(mock(Context.class));
+    SQLiteDatabase db = mock(SQLiteDatabase.class);
+    dbHelper.onCreate(db);
+    verify(db).execSQL(Mockito.startsWith("CREATE TABLE " + TABLE_APPLICATION));
+    verify(db).execSQL(Mockito.startsWith("CREATE TABLE " + TABLE_RULE));
+    verify(db).execSQL(Mockito.startsWith("CREATE VIEW " + VIEW_APPLICATION_RULE));
+    verifyNoMoreInteractions(db);
+  }
+
+  @Test
+  public void onUpgradeV1ToV1() {
+    DbHelper dbHelper = new DbHelper(mock(Context.class));
+    SQLiteDatabase db = mock(SQLiteDatabase.class);
+    dbHelper.onUpgrade(db, 1, 1);
+    verifyNoInteractions(db);
+  }
+}
